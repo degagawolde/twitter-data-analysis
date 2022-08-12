b2cb14f8f9df89c5ed8da669c1ac5e4f23d430b0
degagawolde <degagawolde@gmail.com>
2022-08-12 23:00:58 +0300

    edit docker

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from os import chdir
 from attr import has
 import pandas as pd
 import numpy as np
+import string
 from wordcloud import STOPWORDS
 
 
@@ -26,6 +27,60 @@ def replace_chars(text, chars):
     for char in chars:
         text = text.replace(char, '')
     return text
+def most_common_words_in_tweet(cleaned_df):
+    cleaned_df['clean_text'] = cleaned_df['clean_text'].astype(str)
+    cleaned_df['clean_text'] = cleaned_df['clean_text'].apply(
+    lambda x: x.lower())
+    cleaned_df['clean_text'] = cleaned_df['clean_text'].apply(
+    lambda x: x.translate(str.maketrans(' ', ' ', string.punctuation)))
+    
+    return ' '.join(cleaned_df.clean_text.values)
+
+
+def get_most_mentioned(cleaned_df):
+    user_mention = cleaned_df['user_mentions'].map(np.array).values
+    all_mentions= []
+    for user in user_mention:
+        user = user.strip("][").split(",")
+        all_mentions += user
+        
+    all_mentions = list(map(lambda s: s.replace(
+        " ", '').replace("'", ""), all_mentions))
+    user_mention = " ".join(all_mentions)
+    chars = ['\ufe0f',
+             '\xe1',
+             '\u0688',
+             '\u062d',
+             '\u06a9',
+             '\u0648',
+             '\u0645',
+             '\u0646',
+             '\u0627',
+             '\u0638',
+             '\u0631',
+             '\u067e',
+             '\u0691',
+             '\u062a',
+             '\u0679',
+             '\u062a',
+             '\u0939',
+             '\u0932',
+             '\u094d',
+             '\u093e',
+             '\u092c',
+             '\u094b',
+             '\u0432',
+             '\u043e',
+             '\u0439',
+             '\u043d',
+             '\u0430',
+             '\u0441',
+             # '\ufe0',
+             # '\xe1',
+
+             ]
+    user_mention = replace_chars(user_mention, chars)
+    return user_mention
 
 def get_common_hashtags(cleaned_df, top):
     hashtags = cleaned_df['hashtags'].map(np.array).values
@@ -76,28 +131,6 @@ def get_common_hashtags(cleaned_df, top):
     # hashtags = hashtags.encode('ascii', 'ignore')
     # print(len(hashtags))
     return hashtags
-
-
-def get_tweet_words(df):
-    # custom_stopwords = ['t', 'rt', 'ti', 'vk', 'to', 'co',
-    #                 'dqlw', 'z', 'nd', 'm', 's', 'kur', 'u', 'o', 'd']#,
-    #                 # "will", "new", "amp"]
-    # STOP_WORDS = STOPWORDS.union(custom_stopwords)
-    df['clean_text'] = df['clean_text'].apply(
-        lambda x: [item for item in x.split()])
-    sentence_list = df['clean_text'].to_list()
-    words = []
-    for ele in sentence_list:
-        for w in ele:
-            words.append(w.strip("#"))
-    # print(STOPWORDS)
-    words = " ".join(words)
-    return words
-    # word_list = [sent for sent in sentence_list]
-    # print(word_list[:5])
-    # texts = df['original_text'].values
-    # print(texts)
-
 
 def retweets_polarity(df: pd.DataFrame) -> pd.DataFrame:
     rt_count = df[df['retweet_count'] > 0]['score'].value_counts()
